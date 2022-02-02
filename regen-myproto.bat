022b0c44c1e1560f66c905ba6bf9993e83c2f5ce
Frank Evans <evansf@users.noreply.github.com>
2022-02-02 15:52:16 -0800

    Add files via upload
    script to update MyProtos

---
+++ regen-myproto.bat
@@ -0,0 +1,1 @@
+cd D:\Data\PROJECTS\CowTrack\SW\MeshTrackGit\Meshtastic-device\proto && ..\nanopb\nanopb-0.4.4\generator-bin\protoc.exe --nanopb_out=-v:..\src\mesh\generated -I=..\proto beaconmap.proto
